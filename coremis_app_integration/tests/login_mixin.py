2d3cec2254d1f54404c9dca492ea93584fd23ed7
sniedzielski <sniedzielski@soldevelo.com>
2023-09-11 14:53:43 +0200

    CM-304: fixed tests - added paymentPoint

--- coremis_app_integration/tests/login_mixin.py
+++ coremis_app_integration/tests/login_mixin.py
@@ -4,6 +4,9 @@ from core.services import (
     create_or_update_core_user
 )
 from coremis_app_integration.utils import DbManagerUtils
+from location.models import Location
+from payroll.models import PaymentPoint
+
 
 
 class LogInMixin:
@@ -30,4 +33,17 @@ class LogInMixin:
             user_id=None, data=self._TEST_DATA_USER, audit_user_id=999, connected=False)
         create_or_update_core_user(
             user_uuid=None, username=self._TEST_DATA_USER["username"], i_user=i_user)
-        return DbManagerUtils.get_object_or_none(User, username=self._TEST_USER_NAME)
+        user = DbManagerUtils.get_object_or_none(User, username=self._TEST_USER_NAME)
+        self.__create_payment_point(user)
+        return user
+
+    def __create_payment_point(self, user, **kwargs):
+        name = "TestPPM",
+        location = Location.objects.filter(validity_to__isnull=True, type='V').first()
+        payment_point = PaymentPoint(
+            name=name,
+            location=location,
+            ppm=user
+        )
+        payment_point.save(username=user.username)
+        return payment_point
