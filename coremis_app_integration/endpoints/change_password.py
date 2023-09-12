2eff7dba831962517a0200f2ac687aa8fd7525b6
sniedzielski <sniedzielski@soldevelo.com>
2023-09-12 13:14:43 +0200

    CM-310: added adjustment related to dataclasses

--- coremis_app_integration/endpoints/change_password.py
+++ coremis_app_integration/endpoints/change_password.py
@@ -1,5 +1,6 @@
 import re
 
+from dataclasses import asdict
 from django.utils.translation import gettext_lazy as _
 from rest_framework.authentication import authenticate
 from rest_framework.decorators import (
@@ -11,11 +12,10 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 
-from coremis_app_integration.authorization import (
-    CustomBasicAuthentication,
-    DETAILS,
-    FAILED,
-    SUCCESS
+from coremis_app_integration.authorization import CustomBasicAuthentication
+from coremis_app_integration.dataclasses import (
+    FailedResponse,
+    SuccessResponse
 )
 
 
@@ -30,36 +30,36 @@ def change_password(request):
     new_password = request.data.get('newpassword', None)
     confirm_password = request.data.get('confirmpassword', None)
     if new_password != confirm_password:
-        response = FAILED
-        response['result'] = _('The new password does not match the confirmation of new one')
-        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+        response = FailedResponse()
+        response.result = _('The new password does not match the confirmation of new one')
+        return Response(asdict(response), status=status.HTTP_400_BAD_REQUEST)
 
     if not re.match(REGEX_PATTERN, new_password):
-        response = FAILED
-        response['result'] = _('The new password does not match the required pattern')
-        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+        response = FailedResponse()
+        response.result = _('The new password does not match the required pattern')
+        return Response(asdict(response), status=status.HTTP_400_BAD_REQUEST)
 
     user = request.user
     authenticated_user = authenticate(username=user.username, password=old_password)
 
     if not authenticated_user:
-        response = FAILED
-        response['result'] = _('Please provide correct old password')
-        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+        response = FailedResponse()
+        response.result = _('Please provide correct old password')
+        return Response(asdict(response), status=status.HTTP_400_BAD_REQUEST)
 
     try:
         user.set_password(new_password)
         user.save()
     except Exception as exc:
-        response = FAILED
-        response['result'] = exc
-        return Response(response, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        response = FailedResponse()
+        response.result = exc
+        return Response(asdict(response), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    response_data = SUCCESS
-    response_data["result"] = {}
-    return Response(response_data, status=status.HTTP_200_OK)
+    response_data = SuccessResponse()
+    response_data.result = _build_success_output()
+    return Response(asdict(response_data), status=status.HTTP_200_OK)
 
 
-def _build_success_output(user):
+def _build_success_output():
     # TODO implement 'success' output once WordlBank team provide better specification for it
-    pass
+    return {}
